1ed109f8636dbfe17491175da7756336517e3504
Vince Lu <vincentlu@hotmail.ca>
2021-11-19 22:33:57 -0500

    fixed checkoutSessionRef variable

--- stripe/createCheckoutSession.js
+++ stripe/createCheckoutSession.js
@@ -5,6 +5,8 @@ import {
   collection,
   addDoc,
   Timestamp,
+  doc,
+  onSnapshot,
 } from "firebase/firestore";
 import { config } from "../components/Auth/config"
 import { getAuth } from "firebase/auth";
@@ -15,13 +17,13 @@ export async function createCheckoutSession(uid) {
   //if (!getApps().length) {
   //  window.firebaseApp = initializeApp(config);
   //}
-  const auth = getAuth(global.firebaseApp);
+  const auth = getAuth();
 
   // Firestore (optional): uncomment the next line if you want to create a database for your users
-  const db = getFirestore(global.firebaseApp);
+  const db = getFirestore();
 
   // Create a new checkout session in the subollection inside this users document
-  const checkoutSessionRef = await 
+  /*const checkoutSessionRef = await 
     collection(db, "users")
     .doc(uid)
     .collection("checkout_sessions")
@@ -29,16 +31,40 @@ export async function createCheckoutSession(uid) {
       price: "price_1Jx2XFLBlaDAR7THC8EfBw0S",
       success_url: window.location.origin,
       cancel_url: window.location.origin,
-    });
+    });*/
+  let newObj = {
+    price: 'price_1Jx2XFLBlaDAR7THC8EfBw0S',
+    success_url: window.location.origin,
+    cancel_url: window.location.origin,
+  };
+  console.log(`DBG: new obj is ${JSON.stringify(newObj, null, 2)}`);
+
+  // Create a new checkout session in the subollection inside this users document
+  let userCollRef = collection(db, "users");
+  let uidDocRef = doc(userCollRef, uid);
+  let checkoutSessCollRef = collection(uidDocRef, "checkout_sessions")
+  const checkoutSessionRef = await addDoc(checkoutSessCollRef, newObj);
+  alert(`${JSON.stringify(checkoutSessionRef)}`);
 
   // Wait for the CheckoutSession to get attached by the extension
-  checkoutSessionRef.onSnapshot(async (snap) => {
+  /*checkoutSessionRef.onSnapshot(async (snap) => {
     const { sessionId } = snap.data();
     if (sessionId) {
       // We have a session, let's redirect to Checkout
       // Init Stripe
       const stripe = await initializeStripe();
       stripe.redirectToCheckout({ sessionId });
+    }
+  });*/
+  const unsub = onSnapshot(doc(db, "users", uid), (doc) => {
+    const { sessionId } = doc.data();
+    alert(`${JSON.stringify(sessionId)}`);
+    if (sessionId) {
+      // We have a session, let's redirect to Checkout
+      // Init Stripe
+      const stripe = initializeStripe();
+      stripe.redirectToCheckout({ sessionId });
+      alert('stripe initialized');
     }
   });
 }
